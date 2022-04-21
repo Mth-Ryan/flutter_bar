2cad7e8655775c1226325c6daf57ab754ea9ab47
Mateus Ryan <mthryan@protonmail.com>
2022-04-21 12:26:17 -0300

    SystemWidget business logic

--- lib/widgets/system.dart
+++ lib/widgets/system.dart
@@ -1,12 +1,35 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_bar/palette.dart';
 import 'base.dart';
-import '../palette.dart';
 
-class SystemWidget extends StatelessWidget {
-  SystemWidget({Key? key}) : super(key: key);
+class SystemWidget extends StatefulWidget {
+  const SystemWidget({Key? key}) : super(key: key);
+  @override
+  State<SystemWidget> createState() => _SystemWidgetState();
+}
 
-  final distro = "Fedora";
-  final icon = CupertinoIcons.f_cursive_circle_fill;
+class _SystemWidgetState extends State<SystemWidget> {
+  late String _id;
+  IconData _icon = CupertinoIcons.f_cursive_circle_fill;
+  String _distro = "Linux";
+
+  @override
+  void initState() {
+    super.initState();
+
+    final idRegex = RegExp(r'^ID=');
+    final f = File("/etc/os-release");
+    if (f.existsSync()) {
+      _id = f
+          .readAsLinesSync()
+          .firstWhere((e) => idRegex.hasMatch(e))
+          .split("=")[1];
+      _distro = "${_id[0].toUpperCase()}${_id.substring(1)}";
+      // TODO: get icon from id;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +37,6 @@ class SystemWidget extends StatelessWidget {
         backgroundColor: Palette.systemBackground,
         onPressed: () {},
         child: BaseContentWidget(
-            color: Palette.foreground, icon: icon, text: distro));
+            color: Palette.leftForeground, icon: _icon, text: _distro));
   }
 }
